b0f4c44d42404ca1100397414c2f84bfc90df77d
Alexandre Diniz <32992263+Alexandre-Diniz@users.noreply.github.com>
2020-12-27 16:30:39 -0300

    maskFunctions

---
+++ maskFunctions.js
@@ -0,0 +1,23 @@
+function cpfMask(cpf){
+  return cpf.replace(/\D/g, '')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+    .replace(/(-\d{2})\d+?$/, '$1')
+}
+
+function phoneMask(phone){
+  return phone.replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{5})(\d)/, '$1-$2')
+    .replace(/(-\d{4})\d+?$/, '$1')
+}
+
+function dateMask(date){
+  return date.replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '$1/$2')
+    .replace(/(\d{2})(\d)/, '$1/$2')
+    .replace(/(\/\d{4})\d+?$/, '$1')
+}
+
+export { cpfMask, phoneMask, dateMask }
